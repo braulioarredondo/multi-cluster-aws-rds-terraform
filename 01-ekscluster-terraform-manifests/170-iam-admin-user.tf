d0b38648dccf26cdde89e896031ed84102c023bc
Braulio <braulio.arredondo@wnco.com>
2023-11-03 18:12:13 -0500

    Adding Github actions jobs

--- 01-ekscluster-terraform-manifests/170-iam-admin-user.tf
+++ 01-ekscluster-terraform-manifests/170-iam-admin-user.tf
@@ -11,3 +11,8 @@ resource "aws_iam_user_policy_attachment" "admin_user" {
   user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+#Since aws_iam_user_login_profile resource does not allow to add a password argument 
+#and password output is not a best practice please create the login credentials with this command 
+
+#aws iam create-login-profile --user-name MyUser --password My!User1Login8P@ssword --password-reset-required
